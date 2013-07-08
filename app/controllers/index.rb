80afd4cb9aaf8fb4852115f9fbdc2f7d3386fac2
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-08 12:02:48 -0700

    added ajax load calls

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -4,16 +4,26 @@
 #   erb :index
 # end
 
+
+get '/:twitter_username/user_tweets' do
+  @user = User.find_or_create_by_twitter_username(params[:twitter_username])
+  #load page
+  erb :user_tweets
+end
+
+post '/retrieve_tweets' do
+  @user = User.find_or_create_by_twitter_username(params[:twitter_username])
+  @tweets = @user.fetch_tweets!
+  erb :_tweet_partial
+end
+
 get '/:twitter_username' do
   @user = User.find_or_create_by_twitter_username(params[:twitter_username])
   if @user.tweets.empty? || @user.tweets_stale?
     # User#fetch_tweets! should make an API call
     # and populate the tweets table
     #
-    # Future requests should read from the tweets table 
-    # instead of making an API call
-    @user.fetch_tweets!
-    @message = "just did that shit"
+    redirect ("/" + @user.twitter_username + "/user_tweets")
   end
 
   @tweets = @user.tweets.limit(10)
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,12 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $('.load-gif').on('load', function(){
+    $.ajax({
+      url: '/retrieve_tweets',
+      method: 'post',
+      data: {twitter_username: window.location.pathname.split('/')[1]}
+    }).done(function(response){
+      $('.load-gif').css('display', 'none');
+      $('.tweet-output').append(response);
+    });
+  });
 });
